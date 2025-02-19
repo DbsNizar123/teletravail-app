5c1ac09c07d4041db6290185bfe2bce3df7349ae
nizar <dabboussinzr@gmail.com>
2025-02-19 10:01:22 -0800

    add permission

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserAuthController extends Controller
 {
     //
-    function login(Request $request){
-        return "login function";
+     function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return ['result'=>"user not found", "success"=>false];
+        }
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+        return ['result'=>$success,'msg'=>"user register successfully"];
     }
+
 
     function signup(Request $request){
         $input = $request->all();
